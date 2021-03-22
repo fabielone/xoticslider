230570c2783d5c49958384e00a6674b045ac20f2
Fabielone <64607173+fabielone@users.noreply.github.com>
2021-03-22 01:05:27 -0700

    Add files via upload

---
+++ xoticslider.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var secondarySlider = new Splide('#secondary-slider', {
+        fixedWidth: 100,
+        height: 60,
+        gap: 10,
+        rewind: true,
+        cover: true,
+        isNavigation: true,
+        pagination: false,
+        focus: 'center',
+        breakpoints: {
+            '600': {
+                fixedWidth: 66,
+                height: 40,
+            }
+        }
+    }).mount();
+    var primarySlider = new Splide('#primary-slider', {
+        type: 'fade',
+        heightRatio: 0.40,
+        pagination: false,
+        arrows: false,
+        cover: true,
+    }); // do not call mount() here.
+
+    primarySlider.sync(secondarySlider).mount();
+
+
+});
